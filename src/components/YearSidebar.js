09a1da0125dfe3cf1e5ba07dea83014c73785b91
Miles <milesweldo@gmail.com>
2023-01-14 16:15:54 -0500

    file cleanup

--- src/components/YearSidebar.js
+++ src/components/YearSidebar.js
@@ -1,11 +1,14 @@
 const YearSidebar = ({ sortedYears }) => {
   return (
-    <div className="year-sidebar p-2 rounded-lg fixed z-10 top-1/4 right-5 bg-zinc-700">
-      <h3 className="text-white">Years</h3>
+    <div className="text-center p-2 rounded-lg fixed z-10 top-1/4 right-5 ml-5 bg-zinc-700">
+      <h3 className="text-white mb-2">Year</h3>
       <ul>
         {sortedYears.map((year) => (
           <li key={year}>
-            <a href={`#${year}`} className="text-white hover:text-gray-200">
+            <a
+              href={`#${year}`}
+              className="text-white hover:text-gray-200 transition duration-100"
+            >
               {year}
             </a>
           </li>
